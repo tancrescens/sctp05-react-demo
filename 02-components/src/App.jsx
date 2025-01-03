5b8ff9bd319b12ce768bb74e0a179be327a677d5
kunxin-chor <chorkunxin@yahoo.com>
2025-01-03 06:38:42 +0000

    Added props example

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
-import apples from "./assets/apples.jpg"
+import apples from "./assets/apples.jpg";
+import "./App.css";
+
+// Create a component that display an image
+// and has a border around it
+function BorderImageFrame() {
+  return (
+
+    /*
+      <img src="apple.jpg" style="border:3px solid red;margin:10px;padding:10px"
+
+    */
+
+    // Inline CSS must be provided as an object
+    <img src={apples} style={{
+      border:"3px solid red",
+      margin:"10px",
+      padding:"10px"
+    }}/>
+  )
+}
+
 
 function ImageFrame() {
   return (
@@ -11,8 +32,22 @@ function ImageFrame() {
 // 1. is a function
 // 2. returns JSX
 // 3. first alphabet is uppercase
-function AlertMessage() {
-  return <div>Awas! Awas! Danger Ahead</div>
+// 4. can recieve parameters in the form of props
+// - props are objects and is the first argument/parameter of all component functions
+function AlertMessage(props) {
+
+
+  const message = props.message;
+  const color = props.bgColor;
+  // 
+  // let el = document.querySelector(".alert"); 
+  // el.style.backgroudColor = "azure";
+
+  return <div style={{
+    padding:"10px",
+    margin:"10px",
+    backgroundColor: props.bgColor
+  }}>{message}</div>
 }
 
 function sayGoodbye() {
@@ -33,9 +68,12 @@ export default function App() {
     {sayHappyNewYear()}
     {/* When we want to call a Component function
     we can treat it as if it is a JSX element */}
-    <AlertMessage/>
+    <AlertMessage message="Login Failed" bgColor="pink"/>
+    <AlertMessage message="Login Successful" bgColor="green"/>
     <ImageFrame/>
     <ImageFrame/>
+    <BorderImageFrame/>
+    <BorderImageFrame/>
     </>
   )
 }
